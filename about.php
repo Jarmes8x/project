44f0a5eed680279bae866a3d34967e6f228e8ade
JARMES <135515503+Jarmes8x@users.noreply.github.com>
2023-11-29 22:38:46 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,133 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/about-img.svg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>Suggestion</h3>
+         <p>Here you can provide feedback about the website or problems you encounter on the website.</p>
+         <a href="contact.php" class="btn">Click here</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+   
+   <h1 class="heading">client's reviews</h1>
+
+   <div class="swiper reviews-slider">
+
+   <div class="swiper-wrapper">
+      
+
+   <?php
+     $select_messages = $conn->prepare("SELECT * FROM `messages` LIMIT 6"); 
+     $select_messages->execute();
+     if($select_messages->rowCount() > 0){
+      while($fetch_messages = $select_messages->fetch(PDO::FETCH_ASSOC)){
+   ?>
+   <form action="" method="post" class="swiper-slide slide">
+      <input type="hidden" name="pid" value="<?= $fetch_messages['id']; ?>">
+      <input type="hidden" name="name" value="<?= $fetch_messages['name']; ?>">
+      <input type="hidden" name="email" value="<?= $fetch_messages['email']; ?>">
+      <input type="hidden" name="message" value="<?= $fetch_messages['message']; ?>">
+      <div class="name"><?= $fetch_messages['name']; ?></div>
+      <div class="message"><?= $fetch_messages['message']; ?></div>
+
+   </form>
+   <?php
+      }
+   }else{
+      echo '<p class="empty">no messages added yet!</p>';
+   }
+   ?>
+
+
+   </div>
+
+   <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+        slidesPerView:1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+</script>
+
+</body>
+</html>
