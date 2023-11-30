2612432180704bd3c3e268ce991db08015a92795
JARMES <135515503+Jarmes8x@users.noreply.github.com>
2023-11-30 19:38:57 +0700

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -96,7 +96,7 @@ if(isset($_POST['order'])){
       </div>
 
       <h3>place your orders</h3>
-      <h3>PP : 0000000000</h3>
+      <h3>Prompt Pay:0000000000</h3>
 
       <div class="flex">
          <div class="inputBox">
